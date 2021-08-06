0c6ed11cba5a74c7f5fc7eacd5591f64f53fde0a
mingyu <cxz3619@korea.ac.kr>
2021-08-06 14:47:03 +0900

    출입 시 소리,홈 화면 디자인 수정

--- src/QRCodeScanner.js
+++ src/QRCodeScanner.js
@@ -11,12 +11,13 @@ import {
   Dimensions,
   Image,
   View,
+  Button
 } from 'react-native';
-
+import  SoundPlayer  from  'react-native-sound-player' 
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
 import moment from 'moment-timezone';
-import {color} from 'react-native-reanimated';
+
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as Animatable from 'react-native-animatable';
@@ -35,24 +36,14 @@ const ScanScreen = () => {
 
   const API_URL = 'http://cxz3619.pythonanywhere.com/';
 
-  const hi = () => {
-    console.log('hi');
-  };
-  const startScan = () => {
-    if (scanner) {
-      scanner._setScanning(false);
-    }
-  };
-
-  const onSuccess = e => {
-    console.log('e.data 타입:', typeof e.data);
-    const userInfo = JSON.parse(e.data);
+    const onSuccess = e => {
+    if(e.data.substring(0,8)=='{"email"'){
+    const userInfo = JSON.parse(e.data); 
     setEmail(userInfo.email.replace('.ac.kr', '').toString()); //ac.kr 꼴 삭제 --> 장고에서 @korea.ac.kr 꼴 인식 못함(http://163.152.223.34:8000/MemberData/cxz3619@korea일떄나 개인 페이지 인식가능 )
     setphotoURL(userInfo.photo); //구글 프로필 이미지
     scanned ? setScanned(false) : setScanned(true); //큐알 인식시 state 바꿔주기
-    const date = new Date();
-    console.log(date);
-  };
+    }
+  }; 
   // 스캐너 초기화  부분
   let scanner;
 
@@ -66,7 +57,6 @@ const ScanScreen = () => {
         .then(data => {
           console.log('data.phnoe_num:', data.phone_num);
           setLoading(false);
-          var time = new Date();
           console.log('user info:', data);
           setUsers(data);
           try {
@@ -92,6 +82,10 @@ const ScanScreen = () => {
                   "API_URL+'liveData/'+phone_num:",
                   API_URL + 'liveData/' + data.phone_num,
                 );
+                console.log("****liveData_data_live",data_live  );
+                console.log("****liveData_data_live_type",typeof data_live  );
+               
+                /// live Data에 전화번호 보내고 있으면 지우고(if문) , 아예 없는 데이터면 error 음내고(else if), 있으면 그대로 넣음(else문) 
                 if (
                   Object.entries(data_live).toString() ==
                   'phone_num,live data with this phone num already exists.'
@@ -107,11 +101,18 @@ const ScanScreen = () => {
                     .catch(
                       error => console.log('Delete_livdData_error:', error), //문제되는 부분[SyntaxError: JSON Parse error: Unexpected EOF]
                     );
+                    SoundPlayer.playSoundFile('out', 'mp3'); //퇴장 시 소리 남 
                 }
 
-                if (data.phone_num) {
-                  // 코로나 기록 테이블로 전송(출입할때 마다)
-                  //covid 실행
+                else if(
+                  Object.entries(data_live).toString() ==
+                  'student_num,This field is required.'
+                ){
+                  SoundPlayer.playSoundFile('error', 'mp3') //데이터 베이스에 없는 사람 출입 시
+                }
+
+                else{
+                  SoundPlayer.playSoundFile('in', 'mp3')    // 정상적인 입장
                   console.log(API_URL + 'covidRecord/');
                   fetch(API_URL + 'covidRecord/', {
                     method: 'POST',
@@ -130,8 +131,32 @@ const ScanScreen = () => {
                     .then(response => response.json())
                     .then(data => console.log('covid_data:', data))
                     .catch(error => console.log('covid_error:', error));
-                  //covid
                 }
+
+                // if (data.phone_num) {
+                //   // 코로나 기록 테이블로 전송(출입할때 마다)
+                //   //covid 실행
+                //   console.log(API_URL + 'covidRecord/');
+                //   fetch(API_URL + 'covidRecord/', {
+                //     method: 'POST',
+                //     headers: {
+                //       Accept: 'application/json',
+                //       'Content-Type': 'application/json',
+                //     },
+                //     body: JSON.stringify({
+                //       phone_num: data.phone_num,
+                //       name: data.name,
+                //       major: data.major,
+                //       student_num: data.student_num,
+                //       enter_time: moment().format('YYYY/MM/DD HH:mm:ss'),
+                //     }),
+                //   })
+                //     .then(response => response.json())
+                //     .then(data => console.log('covid_data:', data))
+                //     .catch(error => console.log('covid_error:', error));
+                //   //covid
+                // }
+
               })
               .catch(error => console.log('liveData_data_Input_error:', error));
           } catch (e) {
@@ -213,9 +238,11 @@ const ScanScreen = () => {
             </View>
 
             <View style={styles.bottomOverlay} />
+            
           </View>
         }
       />
+
     </View>
   );
 };
--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   View,
@@ -13,15 +13,15 @@ import {
   Image,
   Dimensions
 } from 'react-native';
-import {baseProps} from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
-import {GoogleSignin} from '@react-native-community/google-signin';
+import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
+import { GoogleSignin } from '@react-native-community/google-signin';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Chart from './chart';
 import ProgressCircle from 'react-native-progress-circle'
 
-const HomeScreen = ({route, navigation}) => {
+const HomeScreen = ({ route, navigation }) => {
   const API_URL = 'http://cxz3619.pythonanywhere.com/liveData';
-  
+
   //const userInfo = route.params; //개인정보
   const [users, setUsers] = useState([]); //현재 몇명있는지 정보
   const [loading, setLoading] = useState(true);
@@ -58,7 +58,7 @@ const HomeScreen = ({route, navigation}) => {
       navigation.reset({ routes: [{ name: 'Login' }] });
       await GoogleSignin.revokeAccess();
       await GoogleSignin.signOut();
-      
+
     } catch (error) {
       Alert.alert('Something else went wrong... ', error.toString());
     }
@@ -66,7 +66,7 @@ const HomeScreen = ({route, navigation}) => {
 
 
   const value = 0.66;
-  
+
   if (loading) {
     return (
       <SafeAreaView style={styles.loading}>
@@ -78,35 +78,55 @@ const HomeScreen = ({route, navigation}) => {
 
     return (
       <SafeAreaView style={styles.container}>
-       
-       <ProgressCircle
-            percent={users.length/50}
-            radius={50}
-            borderWidth={8}
-            color="#3399FF"
-            shadowColor="#999"
-            bgColor="#fff"
-        >
-            <Text style={{ fontSize: 18 }}>{users.length} /50</Text>
-        </ProgressCircle>
+
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.container}>
+            <View style={styles.radiusbar}>
+              <Text style={{ color: "white" }}>실시간 이용자</Text>
+            </View>
+
+            <ProgressCircle
+              percent={users.length / 50}
+              radius={50}
+              borderWidth={8}
+              color="#3399FF"
+              shadowColor="#999"
+              bgColor="#fff"
+
+            >
+              <Text style={{ fontSize: 18 }}>{users.length} /50</Text>
+            </ProgressCircle>
+
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.radiusbar}>
+              <Text style={{ color: "white" }}>유저 프로필</Text>
+            </View>
+            <Image style={{ width: 100, height: 100 }} source={{ uri: userPhoto }} />
+          </View>
+
+        </View>
+
 
         <View style={styles.content}>
-          <Text>
-            {/* {'\n'}email:{JSON.stringify(userEmail)} */}
-            {'\n'}
-          </Text>
-          <Image style={{width: 30, height: 30}} source={{uri: userPhoto}} />
 
-          <Text>
+
+
+          {/* <Text>
             {'\n'}현재 사용자 목록:{'\n'}
-          </Text>
+          </Text> */}
 
-          <Text>
+          {/* <Text>
             {' '}
             현재 {users.length} 명 사용중 {'\n'}
-          </Text>
-         
-<Chart/>
+          </Text> */}
+
+
+          <View style={styles.radiusbar}>
+            <Text style={{ color: "white" }}>이용시간 분포</Text>
+          </View>
+          <Chart />
 
 
           <View>
@@ -133,10 +153,10 @@ const HomeScreen = ({route, navigation}) => {
               color="black"
               title="map"
               onPress={() =>
-               navigation.navigate('Map')
+                navigation.navigate('Map')
               }
             />
-          
+
           </View>
 
         </View>
@@ -147,12 +167,23 @@ const HomeScreen = ({route, navigation}) => {
 };
 
 const styles = StyleSheet.create({
-  
+
+  radiusbar: {
+    width: 100,
+    height: 20,
+    backgroundColor: "#A33B39",
+    borderRadius: 40,
+    alignItems: "center",
+    marginBottom: 5
+  }
+  ,
+
   container: {
     flex: 1,
-    //justifyContent: 'center',
-    //alignItems: 'center',
-    paddingTop: StatusBar.currentHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: StatusBar.currentHeight,
+
     //height: 1000,
   },
   loading: {
@@ -163,6 +194,7 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: 'center',
+    
   },
   footer: {
     height: 60,
@@ -204,7 +236,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     backgroundColor: '#A33B39',
   },
-  
+
 });
 
 
